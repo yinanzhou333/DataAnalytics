6360f432c2f76f49929eab472a4406d982c84287
yinan <yinan@myvm.myguest.virtualbox.org>
2024-02-17 22:40:42 -0800

    kaggle dataset

---
+++ data_analysis.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(lubridate)
+library(janitor)
+library(skimr)
+library(psych)
+library(ggplot2)
+library(reshape2)
+library(DataExplorer)
+library(scales)
+
+features<-read.csv("Features_data_set.csv", header = TRUE)
+sales<-read.csv("sales_data_set.csv", header = TRUE)
+stores<-read.csv("stores_data_set.csv", header = TRUE)
+
+head(stores)
+head(features)
+head(sales)
+
+glimpse(stores)
+glimpse(features)
+glimpse(sales)
